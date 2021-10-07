72e498965db57aea5a89837d9bbe57da6d69fb76
c0de4un <code4un@yandex.ru>
2021-10-07 22:18:54 +0400

    initial commit

---
+++ cmake/compiler.cmake
@@ -0,0 +1,27 @@
+# = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# COMPILER
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+# C++ Version
+set( CMAKE_CXX_STANDARD ${PROJECT_CXX_VERSION} )
+set( CMAKE_CXX_STANDARD_REQUIRED ON )
+
+set( CMAKE_INCLUDE_CURRENT_DIR ON )
+
+set( CMAKE_AUTOUIC ON )
+set( CMAKE_AUTOMOC ON )
+set( CMAKE_AUTORCC ON )
+
+# Flags
+if ( CMAKE_CXX_COMPILER_ID STREQUAL "Clang" )
+    set( CMAKE_CXX_FLAGS "-fexceptions -fcxx-exceptions" ${CMAKE_CXX_FLAGS} )
+endif ( CMAKE_CXX_COMPILER_ID STREQUAL "Clang" )
+
+# Enable Position Independent Code
+set( CMAKE_POSITION_INDEPENDENT_CODE ON )
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+# = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
